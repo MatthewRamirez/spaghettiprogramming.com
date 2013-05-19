b2c6f4b8003f677148d70e1727840d35ad060e1a
Matthew Ramirez <matthew.ramirez@gmail.com>
2013-05-19 04:04:01 +0000

    clean up routes a bit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 SpaghettiprogrammingCom::Application.routes.draw do
   resources :sessions, :only => [:new, :create, :destroy]
   resources :users, :only => [ :new, :create ]
-  resources :advertisements, :only => [ :show, :destroy ]
   resources :blog_posts
   get '/blog/:slug' => 'blog_posts#slug', :as => 'blog_slug'
   get '/unpublished' => 'blog_posts#unpublished_index', :as => 'unpublished_blog_posts'
@@ -12,9 +11,9 @@ SpaghettiprogrammingCom::Application.routes.draw do
 
   get '/home', :to => 'pages#home'
   get '/', :to => 'pages#home'
-  match '/signin', :to => 'sessions#new'
-  match '/signout', :to => 'sessions#destroy'
-  match '/auth/:provider/callback' => 'sessions#create'
+  get '/signin', :to => 'sessions#new'
+  get '/signout', :to => 'sessions#destroy'
+  get '/auth/:provider/callback' => 'sessions#create'
   root :to => 'blog_posts#index'
 
 end
