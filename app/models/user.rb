91db9309d6002dede84a586571bb720df5cb041f
Matthew Ramirez <matthew.ramirez@gmail.com>
2013-05-25 22:24:44 +0000

    improve readability in the User.create_from_auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,7 @@ class User < ActiveRecord::Base
   has_many :blog_posts
 
   def self.create_from_auth(user_params)
-    create! do |user|
-      user.uid = user_params[:uid]
-      user.nick = user_params[:nick]
-      user.email_address = user_params[:email_address]
-      user.identifier_url = user_params[:identifier_url]
-      user.provider = user_params[:provider]
-    end
+    create!(user_params)
   end
 end
 
