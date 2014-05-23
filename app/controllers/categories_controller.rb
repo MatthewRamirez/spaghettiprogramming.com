05cac5460891e670c7a57f4435183f62c37ac2b0
Matthew Ramirez <matthew.ramirez@gmail.com>
2014-05-23 15:06:37 -0400

    Use Strong Parameters in category controller.
    Modify the categories controller to use Rails 4 Strong Parameters
    convention when creating and editing new categories.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -18,7 +18,7 @@ class CategoriesController < ApplicationController
   end
 
   def create
-    category = Category.new params[:category]
+    category = Category.new(category_params)
     if category.save
       redirect_to categories_path
     end
@@ -30,7 +30,7 @@ class CategoriesController < ApplicationController
 
   def update
     @category = Category.find params[:id]
-    if @category.update_attributes params[:category]
+    if @category.update_attributes(category_params)
       redirect_to categories_path
     else
       render 'edit'
@@ -42,6 +42,12 @@ class CategoriesController < ApplicationController
     if @category.destroy
       redirect_to categories_path
     end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 
 end
