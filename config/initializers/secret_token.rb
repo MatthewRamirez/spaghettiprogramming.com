846642f67e889a1cb9d3d0a650784835e6bb53d6
Matthew Ramirez <matt@spaghettiprogramming.com>
2015-08-11 13:12:40 -0400

    Add secret key base
    Add secret key base to application configuration for encryption of
    cookies.  See for more info:
    
    http://guides.rubyonrails.org/upgrading_ruby_on_rails.html#action-pack

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 SpaghettiprogrammingCom::Application.config.secret_token = APP_CONFIG["SECRET_TOKEN"]
+SpaghettiprogrammingCom::Application.config.secret_key_base = APP_CONFIG["SECRET_KEY_BASE"]
