a8d76a3c744f80b8dcabaf971a68fe720b37c221
Matthew Ramirez <matt@spaghettiprogramming.com>
2016-08-31 20:01:36 -0400

    Remove backup from database recipe
    This is already being taken care of in the backup recipe.

--- config/provision/site-cookbooks/web_server/recipes/database.rb
+++ config/provision/site-cookbooks/web_server/recipes/database.rb
@@ -20,23 +20,3 @@ postgresql_database 'spaghettiprogramming_production' do
   name database_name
   owner database_user
 end
-
-directory '/var/backups/postgresql' do
-  owner user_id
-  group user_id
-  mode '0700'
-end
-
-cron "backup" do
-  action :create
-  user "postgres"
-  # mailto
-
-  shell "/bin/bash"
-  path "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
-
-  minute "0"
-  hour "1"
-
-  command "pg_dump -ROx #{database_name} | gzip -c > /var/backups/postgresql/#{database_name}-`date +\%d`.sql.gz"
-end
